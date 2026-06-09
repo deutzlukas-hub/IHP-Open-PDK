0d63943e582e2d31f0dd644b77f300a6dc98e9f6
Lukas Deutz <deutzlukas@protonmail.com>
2026-06-09 15:46:10 +0100

    bug fix

--- ihp-sg13g2/libs.tech/gnucap/benchmarks/ngspice/benchmark_inv_chain.py
+++ ihp-sg13g2/libs.tech/gnucap/benchmarks/ngspice/benchmark_inv_chain.py
@@ -3,21 +3,15 @@ from benchmark import Benchmarker
 from base_generator import ModelType
 from gen_inv_chain import InverterChainGenerator
 
-def benchmark_inv_chain(model_type: ModelType):
+def benchmark_inv_chain():
 
     configs = [{"num_inv": num_inv} for num_inv in [10, 50, 100, 500, 1000]]
-    out_name = f"bm_moslv_inv_chain_tt_{model_type}.csv"
-
     gen = InverterChainGenerator()
     bm = Benchmarker(build_dir)
-    bm.run(
-        gen,
-        ModelType.GENERIC,
-        configs,
-        out_name
-    )
+
+    for model_type in [ModelType.GENERIC, ModelType.PARAMSET]:
+        out_name = f"bm_moslv_inv_chain_tt_{model_type}.csv"
+        bm.run(gen, model_type, configs, out_name)
 
 if __name__ == "__main__":
-
-    benchmark_inv_chain(ModelType.GENERIC)
-    benchmark_inv_chain(ModelType.PARAMSET)
+    benchmark_inv_chain()
