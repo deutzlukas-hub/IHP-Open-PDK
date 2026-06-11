6e40877840590fe585b17b95691152dce0630499
Lukas Deutz <deutzlukas@protonmail.com>
2026-06-11 22:28:16 +0100

    fix build.sh

--- ihp-sg13g2/libs.tech/benchmark/verilog-a/build.sh
+++ ihp-sg13g2/libs.tech/benchmark/verilog-a/build.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # build from generic
-openvaf-r ../psp103/psp103.va ../osdi/psp103.osdi
+openvaf-r ./psp103/psp103.va -o ../osdi/psp103.osdi
 # build from paramset
 openvaf-r sg13g2_moslv_paramset_tt_nolocal.dump.va -o ../osdi/sg13g2_moslv.osdi
 # build from tayolored paramset
