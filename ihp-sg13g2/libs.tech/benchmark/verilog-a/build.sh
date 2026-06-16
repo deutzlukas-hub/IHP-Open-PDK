ea3966b691c274a1819898da09ac131aa1011115
Lukas Deutz <deutzlukas@protonmail.com>
2026-06-16 13:19:27 +0100

    comment out debug build

--- ihp-sg13g2/libs.tech/benchmark/verilog-a/build.sh
+++ ihp-sg13g2/libs.tech/benchmark/verilog-a/build.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 # build from generic
 openvaf-r ./psp103/psp103.va -o ../osdi/psp103.osdi
-# build dummped generic for debugging
-openvaf-r psp103.dump.va -o ../osdi/psp103_from_dump.osdi
+## build dummped generic for debugging
+#openvaf-r psp103.dump.va -o ../osdi/psp103_from_dump.osdi
 # build from paramset dump
 openvaf-r sg13g2_moslv_paramset_tt_nolocal.dump.va -o ../osdi/sg13g2_moslv.osdi
 # build from multiplier paramset dump
