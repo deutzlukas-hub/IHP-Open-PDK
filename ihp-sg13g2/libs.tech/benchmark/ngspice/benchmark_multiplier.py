4cfbd66992df04cb9ed66c3b9c9fff90fdd56fb3
Lukas Deutz <deutzlukas@protonmail.com>
2026-06-18 13:07:46 +0100

    changes

--- ihp-sg13g2/libs.tech/benchmark/ngspice/benchmark_multiplier.py
+++ ihp-sg13g2/libs.tech/benchmark/ngspice/benchmark_multiplier.py
@@ -15,21 +15,16 @@ def main():
 
     args = parse_args()
 
-    # Configuration
-    num_invs = [10, 20, 50, 100, 200, 500, 1000]
-
     # Benchmark directory
     benchmark_dir = Path(__file__).parent
 
-    for num_inv in num_invs:
+    # Run generic model
+    # generic_target = f"bench-tb_moslv_rf_c6288_tt_generic"
+    # run_benchmark(generic_target, args.j, args.REPEATS, benchmark_dir)
 
-        # Run generic model
-        generic_target = f"bench-tb_moslv_rf_inv_chain_N{num_inv}_tt_generic"
-        run_benchmark(generic_target, args.j, args.REPEATS, benchmark_dir)
-
-        # Run tailored model
-        tailored_target = f"bench-tb_moslv_rf_inv_chain_N{num_inv}_tt_tailored"
-        run_benchmark(tailored_target, args.j, args.REPEATS, benchmark_dir)
+    # Run tailored model
+    tailored_target = f"bench-tb_moslv_rf_c6288_tt_tailored"
+    run_benchmark(tailored_target, args.j, args.REPEATS, benchmark_dir)
 
 
 if __name__ == "__main__":
